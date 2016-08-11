e843b8a163ed2c26bc227732619f74c7542287c4
josephkent <joseph@contractiq.in>
2016-08-11 13:12:02 +0530

    Initialize repository

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+
+gem 'rails',        '4.0.0'
+
+gem 'sqlite3'
+
+gem 'puma',         '3.4.0'
+
+gem 'sass-rails',   '~> 4.0.0'
+
+gem 'uglifier',     '>= 1.3.0'
+
+gem 'coffee-rails', '~> 4.0.0'
+
+gem 'jquery-rails'
+
+gem 'jbuilder',     '~> 1.2'
+
+group :development do
+  gem "better_errors"
+  gem 'binding_of_caller', '0.7.2'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ToyApplication::Application.config.secret_key_base = '1fc76b9f8920a02d5f6451fd904d6f797c4ebb6c96a1f1e7ad0060e7c48dc5281dc9f38257a52248cefcd679a22fa25d46448d3eaf917c4df3d7c895858e4806'
